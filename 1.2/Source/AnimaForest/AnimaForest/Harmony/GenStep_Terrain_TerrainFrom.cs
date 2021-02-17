876e8ac2b808f0272925f65d4bb241be07b6c75e
juanosarg <31105173+juanosarg@users.noreply.github.com>
2021-02-17 16:49:26 +0100

    v0.1

---
+++ 1.2/Source/AnimaForest/AnimaForest/Harmony/GenStep_Terrain_TerrainFrom.cs
@@ -0,0 +1,37 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System.Reflection;
+using Verse;
+using System.Collections.Generic;
+using RimWorld.Planet;
+using System.Linq;
+using System;
+using RimWorld.BaseGen;
+
+
+namespace AnimaForest
+{
+
+    /*This Harmony Postfix allows us to remove gravel from a certain biome
+    */
+    [HarmonyPatch(typeof(GenStep_Terrain))]
+    [HarmonyPatch("TerrainFrom")]
+    public static class AnimaForest_GenStep_Terrain_TerrainFrom_Patch
+    {
+        [HarmonyPostfix]
+        public static void RemoveGravel(Map map, ref TerrainDef __result)
+
+        {
+            
+            if ((__result == TerrainDefOf.Gravel) && (map.Biome.defName == "RG_AnimaForest"))
+            {
+                //Log.Message("Detectado e intentando cambiar");
+                __result = TerrainDef.Named("RG_CrackedAnimaSoil");
+            }
+
+        }
+
+    }
+
+
+}
---
+++ 1.2/Source/AnimaForest/AnimaForest/Harmony/World_NaturalRockTypesIn.cs
@@ -0,0 +1,79 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System.Reflection;
+using Verse;
+using System.Collections.Generic;
+using RimWorld.Planet;
+using System.Linq;
+using System;
+using RimWorld.BaseGen;
+
+
+namespace AnimaForest
+{
+
+    /*This Harmony Postfix allows us to create new rock types than only appear in certain biomes
+    */
+    [HarmonyPatch(typeof(World))]
+    [HarmonyPatch("NaturalRockTypesIn")]
+    public static class AnimaForest_World_NaturalRockTypesIn_Patch
+    {
+
+       
+        [HarmonyPostfix]
+        public static void MakeRocksAccordingToBiome(int tile, ref World __instance, ref IEnumerable<ThingDef> __result)
+
+        {
+            if ((__instance.grid.tiles[tile].biome.defName == "BiomesIslands_Atoll"))
+            {
+                return;
+            }
+
+            if ((__instance.grid.tiles[tile].biome.defName.Contains("AB_")))
+            {
+                return;
+            }
+
+            if (__instance.grid.tiles[tile].biome.defName == "RG_AnimaForest")
+            {
+                List<ThingDef> replacedList = new List<ThingDef>();
+                ThingDef item = DefDatabase<ThingDef>.GetNamed("RG_Jadeite");
+                replacedList.Add(item);
+
+                __result = replacedList;
+            }
+            else
+            {
+                Rand.PushState();
+                Rand.Seed = tile;
+                List<ThingDef> list = (from d in DefDatabase<ThingDef>.AllDefs
+                                       where d.category == ThingCategory.Building && d.building.isNaturalRock && !d.building.isResourceRock &&
+                                       !d.IsSmoothed && d.defName != "GU_RoseQuartz" && d.defName != "AB_Mudstone" && d.defName != "AB_SlimeStone" &&
+                                       d.defName != "GU_AncientMetals" && d.defName != "AB_Cragstone" && d.defName != "AB_Obsidianstone" && d.defName != "BiomesIslands_CoralRock"
+                                       && d.defName != "RG_Jadeite"
+                                       select d).ToList<ThingDef>();
+                int num = Rand.RangeInclusive(2, 3);
+                if (num > list.Count)
+                {
+                    num = list.Count;
+                }
+                List<ThingDef> list2 = new List<ThingDef>();
+                for (int i = 0; i < num; i++)
+                {
+                    ThingDef item = list.RandomElement<ThingDef>();
+                    list.Remove(item);
+                    list2.Add(item);
+                }
+                Rand.PopState();
+                __result = list2;
+
+
+            }
+
+
+
+        }
+    }
+
+
+}
